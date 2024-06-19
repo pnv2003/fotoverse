d5eb50f9b15dbbdbb9b0273a9e2f3c23418bd8ea
Phuong Ngo <phuongngovan2003@gmail.com>
2024-06-19 10:16:53 +0700

    Add navbar logic

---
+++ client/scripts/navbar.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector("nav");
+
+navbar.setAttribute(
+    "style",
+    "background: linear-gradient(90deg, rgba(9,38,121,1) 0%, rgba(0,212,255,1) 100%);"
+);
+navbar.setAttribute("data-bs-theme", "dark");
+
+// TODO
+profileNav = document.querySelector("#nav-profile");
+profileNav.setAttribute("href", "../profile/index.html?id=" + 1) 
