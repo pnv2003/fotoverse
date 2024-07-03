44fd92c2c58ff39a850e73da7eeff14c6d3059fe
Phuong Ngo <phuongngovan2003@gmail.com>
2024-07-03 08:38:13 +0700

    Update route name for signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
+  get 'signup', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
