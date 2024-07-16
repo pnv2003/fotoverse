89840293336e6e1e19655ba39e9c93ff86d572cd
Phuong Ngo <phuongngovan2003@gmail.com>
2024-07-16 13:35:50 +0700

    Devise: forgot password

---
+++ app/javascript/forgot_password.js
@@ -0,0 +1,13 @@
+import { validate, validator } from "./utils/validate";
+
+const email = document.querySelector("#user_email");
+const emailError = document.querySelector("#user_email + .error");
+const send = document.querySelector("#send");
+
+send.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (validate(email, emailError, validator.email)) {
+        document.querySelector("main.forgot-password form").submit();
+    }
+});
