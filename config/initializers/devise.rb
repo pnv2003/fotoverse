6e14c3c264c60ccde6b093951ecf6f1f1f16a57f
Phuong Ngo <phuongngovan2003@gmail.com>
2024-07-16 11:04:51 +0700

    Change password length

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -178,7 +178,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 6..128
+  config.password_length = 1..64
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
@@ -207,7 +207,7 @@ Devise.setup do |config|
   # config.unlock_strategy = :both
 
   # Number of authentication tries before locking an account if lock_strategy
-  # is failed attempts.
+  # is failed attempts.length
   # config.maximum_attempts = 20
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
