f35470fc0d0287c028dff76a572626e4e22d7241
Phuong Ngo <phuongngovan2003@gmail.com>
2024-07-17 11:45:55 +0700

    Delete photo

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -28,6 +28,11 @@ class Admin::PhotosController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    render json: { status_code: 200, message: "Post deleted." }, status: :ok
+  rescue StandardError => e
+    render json: { status_code: 500, message: e.message }, status: :internal_server_error
   end
 
   private
