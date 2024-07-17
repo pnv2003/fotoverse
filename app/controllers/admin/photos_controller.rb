efb5b2c6e803d5e1ab642269fcaf6da6040a0619
Phuong Ngo <phuongngovan2003@gmail.com>
2024-07-17 11:39:16 +0700

    feat: manage photos

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -3,6 +3,7 @@ class Admin::PhotosController < ApplicationController
   layout "admin"
 
   def index
+    @photos = Photo.all
   end
 
   def edit
@@ -10,8 +11,27 @@ class Admin::PhotosController < ApplicationController
   end
 
   def update
+    @photo = Photo.find(params[:id])
+
+    photo_parameters = photo_params
+    if photo_params[:medium_attributes][:url] != nil
+      @photo.medium.destroy
+    else
+      photo_parameters.delete(:medium_attributes)
+    end
+
+    if @photo.update(photo_parameters)
+      redirect_to user_path(current_user.id, tab: "photos"), flash: {success: "Photo updated successfully"}
+    else
+      redirect_to edit_photo_path(@photo), flash: {error: "Photo update failed: " + @photo.errors.full_messages.join(",")}
+    end
   end
 
   def destroy
+  end
+
+  private
+  def photo_params
+    params.require(:photo).permit(:title, :mode, :description, medium_attributes: [:url, :_destroy])
   end
 end
--- app/javascript/admin_photos.js
+++ app/javascript/admin_photos.js
@@ -1,12 +1,63 @@
+import { getRelativeTime } from "./utils/datetime";
+import { compactFormatter } from "./utils/number";
+
 // modal
 const photoItems = document.querySelectorAll("#photos .item");
 const albumItems = document.querySelectorAll("#albums .item");
 
-const photoModal = new bootstrap.Modal(document.querySelector("#photoModal"), {keyboard: false});
+const photoModal = document.querySelector("#photoModal");
+const photoModalInstance = new bootstrap.Modal(photoModal, {keyboard: false});
 photoItems.forEach(item => item.addEventListener("click", () => {
-    photoModal.show();
-}));
 
+    const userName = item.querySelector(".user-name").textContent;
+    const userAvatar = item.querySelector(".user-avatar img").src;
+    const userPath = item.querySelector(".user-path").textContent;
+    const imgSrc = item.querySelector("img").src;
+    const title = item.querySelector(".title").textContent;
+    const desc = item.querySelector(".desc").textContent;
+    const mode = item.querySelector(".mode").textContent;
+    const reactCount = item.querySelector(".react-count").textContent;
+    const updatedAt = item.querySelector(".updated-at").textContent;
+    
+    photoModal.querySelector(".modal-title .name").textContent = userName;
+    photoModal.querySelector(".modal-title img").src = userAvatar;
+    photoModal.querySelector(".modal-title a").href = userPath;
+    photoModal.querySelector(".modal-body img").src = imgSrc;
+    photoModal.querySelector(".card-title").textContent = title;
+    photoModal.querySelector(".card-text").textContent = desc;
+    if (mode == "private") {
+        photoModal.querySelector(".badge").style.display = "inline";
+    } else {
+        photoModal.querySelector(".badge").style.display = "none";
+    }
+    photoModal.querySelector(".react span").textContent = compactFormatter.format(reactCount);
+    photoModal.querySelector(".ago").textContent = getRelativeTime(new Date(updatedAt));
+
+    // add edit link
+    const editPath = item.querySelector(".edit-path").textContent;
+    document.querySelector("#edit-photo").href = editPath;
+
+    // add delete event
+    const detelePath = item.querySelector(".delete-path").textContent;
+    document.querySelector("#delete-photo").addEventListener("click", () => {
+        http.delete(detelePath, {}, null).then((response) => {
+            const url = new URL(window.location.href);
+            http.get(url.pathname, { tab: "photo", notice: response.message }, null).then(() => {
+                window.location.reload();
+            });
+        });
+    });
+
+    // add reaction info    
+    const photoId = item.querySelector(".id").textContent;
+    const reacted = item.querySelector(".reacted").textContent;
+    const reactButton = photoModal.querySelector(".react");
+    if (reacted === "true") {
+        toggleReact(reactButton.querySelector("i"));
+    }
+    reactButton.setAttribute("data-post-id", photoId);
+    photoModalInstance.show();
+}));
 // pagination
 const pagination = document.querySelector("main nav .pagination");
 
