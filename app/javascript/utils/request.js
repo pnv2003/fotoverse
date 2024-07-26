5e074e06564b65ca42a9e4abab02c8ff21ecacdf
Phuong Ngo <phuongngovan2003@gmail.com>
2024-07-26 08:57:44 +0700

    Config JavaScript environment

--- app/javascript/utils/request.js
+++ app/javascript/utils/request.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "http://127.0.0.1:3000";
+const SERVER_URL = railsEnv === 'development' ? 'http://127.0.0.1:3000' : 'https://fotoverse.onrender.com';
 
 const http = {
     request: async function(method, baseURL, param, data, json = true) {
